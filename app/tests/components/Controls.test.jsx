3dc4020b112cd5b726e1650d9386cf72b1e4c356
johnnyqbui <johnnyqbui7@gmail.com>
2016-10-27 20:16:04 -0500

    added controls component and test

---
+++ app/tests/components/Controls.test.jsx
@@ -0,0 +1,30 @@
+const React = require('react');
+const ReactDOM = require('react-dom');
+const expect = require('expect');
+const $ = require('jQuery');
+const TestUtils = require('react-addons-test-utils');
+
+const Controls = require('Controls');
+
+describe('Controls', () => {
+	it('should exist', () => {
+		expect(Controls).toExist();
+	});
+
+	describe('render', () => {
+		it('should render pause when started', () => {
+			const controls = TestUtils.renderIntoDocument(<Controls countdownStatus='started'/>);
+			const $el = $(ReactDOM.findDOMNode(controls));
+			const $pauseBtn = $el.find('button:contains(Pause)');
+
+			expect($pauseBtn.length).toBe(1);
+		});
+		it('should render start when paused', () => {
+			const controls = TestUtils.renderIntoDocument(<Controls countdownStatus='paused'/>);
+			const $el = $(ReactDOM.findDOMNode(controls));
+			const $startBtn = $el.find('button:contains(Start)');
+
+			expect($startBtn.length).toBe(1);
+		});
+	});
+});
